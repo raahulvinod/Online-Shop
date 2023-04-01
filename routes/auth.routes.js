c4ecf7002c2d5df0525351edf5db4e842772bebb
Rahul vinod <rahulvinod135@gmail.com>
2023-04-01 12:55:20 +0530

    added css variables, website themes and basic styles

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,6 +6,8 @@ const router = express.Router();
 
 router.get('/signup', authContoller.getSignup);
 
+router.post('/signup', authContoller.signup);
+
 router.get('/signup', authContoller.getLogin);
 
 module.exports = router;
