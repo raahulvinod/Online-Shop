973a62b080dd4a4275ff859d0bf872157175b7ca
Rahul vinod <rahulvinod135@gmail.com>
2023-04-04 20:03:19 +0530

    displaying error messages and saving user inputs

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,12 +4,27 @@ const validation = require('../util/validation');
 const sessionFlash = require('../util/session-flash');
 
 function getSignup(req, res) {
-    res.render('customer/auth/signup')
+    let sessionData = sessionFlash.getSessionData(req);
+
+    if (!sessionData) {
+        sessionData = {
+            email: '',
+            confirmEmail: '',
+            password: '',
+            fullname: '',
+            street: '',
+            postal: '',
+            city: ''
+        };
+    }
+
+    res.render('customer/auth/signup', { inputData: sessionData })
 }
 
 async function signup(req, res, next) {
     const enteredData = {
         email: req.body.email,
+        confirmEmail: req.body['confirm-email'],
         password: req.body.password,
         fullname: req.body.fullname,
         street: req.body.street,
@@ -73,7 +88,15 @@ async function signup(req, res, next) {
 }
 
 function getLogin(req, res) {
-    res.render('customer/auth/login');
+    let sessionData = sessionFlash.getSessionData(req);
+
+    if (!sessionData) {
+        sessionData = {
+            email: '',
+            password: ''
+        };
+    }
+    res.render('customer/auth/login', { inputData: sessionData });
 }
 
 async function login(req, res, next) {
@@ -88,8 +111,8 @@ async function login(req, res, next) {
 
     const sessionErrorData = {
         errorMessage: 'Invalid credentials - please double-check your email and password',
-            email: user.email,
-            password: user.password
+        email: user.email,
+        password: user.password
     }
 
     if (!existingUser) {
@@ -102,7 +125,7 @@ async function login(req, res, next) {
     const passwordIsCorrect = await user.hasMatchingPassword(existingUser.password);
 
     if (!passwordIsCorrect) {
-        sessionFlash.flashDataToSession(req, sessionErrorData, function() {
+        sessionFlash.flashDataToSession(req, sessionErrorData, function () {
             res.redirect('/login');
         })
         return;
