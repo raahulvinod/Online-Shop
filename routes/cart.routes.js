7d344c88af38dcf35545a139a0ea2b3e68c45487
Rahul vinod <rahulvinod135@gmail.com>
2023-04-09 14:57:01 +0530

    added cart item via ajax

---
+++ routes/cart.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const cartContoller = require('../controllers/cart.controller');
+
+const router = express.Router();
+
+router.post('/items', cartContoller.addCartItem);
+
+
+module.exports = router;
